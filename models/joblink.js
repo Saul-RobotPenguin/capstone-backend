397a0c948228d10c762642e71e5850d4d5de9315
Saul Calleja <scalleja12345@gmail.com>
2022-06-22 20:04:38 -0400

    Completed backend

--- models/joblink.js
+++ models/joblink.js
@@ -4,11 +4,12 @@ const Schema = mongoose.Schema;
 const Joblink = new Schema(
   {
     username: { type: String, required: true },
-    ownerId: { type: String, required: true },
+    ownerId: { type: String },
     role: { type: String, required: true },
     link: { type: String, required: true },
   },
-  { timestamps: true },
+
+  // { timestamps: true },
   { collection: "jobLinks" }
 );
 
